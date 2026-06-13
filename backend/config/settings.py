b6b987148aa72606f72f7bb896bb3e3aa74d3208
utsho <utshoroy5@gmail.com>
2026-06-13 14:52:40 +0600

    Update

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -170,9 +170,11 @@ CORS_ALLOW_CREDENTIALS = True
 # EMAIL
 # =========================
 
+# EMAIL
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
 EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
